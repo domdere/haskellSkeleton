5124ec09aaea48dfbc2604b80ba2b56b75059bac
Dom De Re <domdere@domdere.com>
2013-05-11 00:46:39 +1000

    WIP: got the compiler to attempt to link the library in, but i still have to work the -rpath flag in there for UNIX and it still isnt copying the darn shared library for the dependent TestCLib project...

--- cmakemodules/HaskellMacros.cmake
+++ cmakemodules/HaskellMacros.cmake
@@ -24,9 +24,9 @@ ENDMACRO ( GET_STATIC_LIBRARY_NAME )
 
 MACRO ( GET_RELOCATABLE_LIBRARY_NAME input outputVar)
     IF ( WIN32 )
-        SET ( ${outputVar} "${input}.dll" )
+        SET ( ${outputVar} "lib${input}.dll" )
     ELSE ( WIN32 )
-        SET ( ${outputVar} "${input}.so" )
+        SET ( ${outputVar} "lib${input}.so" )
     ENDIF ( WIN32 )
 ENDMACRO ( GET_RELOCATABLE_LIBRARY_NAME )
 
@@ -156,8 +156,11 @@ MACRO ( ADD_HASKELL_EXECUTABLE_TARGET projectName )
         ${${projectName}_EXECUTABLE}
         ${ARGN} )
 
-    ADD_CUSTOM_TARGET ( ${projectName} ALL 
+    ADD_CUSTOM_TARGET ( ${projectName}-executable ALL 
         DEPENDS "${${projectName}_EXECUTABLE}" )
+
+    ADD_CUSTOM_TARGET ( ${projectName} ALL 
+        DEPENDS "${projectName}-executable" )
 
     ADD_README_TARGET ( ${projectName} )
 
@@ -192,6 +195,7 @@ MACRO ( CABAL_TARGET projectName )
             "${ROOT_SRC_DIR}/${projectName}/package.json"
             "${ROOT_BIN_DIR}/${projectName}/${projectName}.cabal"
         DEPENDS "${ROOT_SRC_DIR}/build-scripts/generateCabal.py"
+            "${ROOT_SRC_DIR}/${projectName}/package.json"
         WORKING_DIRECTORY "${ROOT_SRC_DIR}/build-scripts/" )
 
 ENDMACRO ( CABAL_TARGET projectName )
@@ -204,12 +208,13 @@ MACRO ( ADD_LICENCE_TARGET projectName license )
         ARGS "-v"
             "${license}" "${ROOT_BIN_DIR}/${projectName}/LICENSE"
         DEPENDS "${ROOT_SRC_DIR}/Licences/${license}"
-        WORKING_DIRECTORY "${ROOT_SRC_DIR}/Licences" )
+        WORKING_DIRECTORY "${ROOT_SRC_DIR}/Licences" 
+        COMMENT "Installing LICENSE file for ${projectName}" )
 
     ADD_CUSTOM_TARGET ( ${projectName}-license ALL 
         DEPENDS "${ROOT_BIN_DIR}/${projectName}/LICENSE" )
 
-    ADD_DEPENDENCIES ( ${projectName} ${projectName}-license )
+    ADD_DEPENDENCIES ( ${projectName}-executable ${projectName}-license )
 
 
 ENDMACRO ( ADD_LICENCE_TARGET projectName license )
@@ -222,12 +227,13 @@ MACRO ( ADD_README_TARGET projectName )
         ARGS "-v"
             "README" "${ROOT_BIN_DIR}/${projectName}/README"
         DEPENDS "${ROOT_SRC_DIR}/${projectName}/README"
-        WORKING_DIRECTORY "${ROOT_SRC_DIR}/${projectName}" )
+        WORKING_DIRECTORY "${ROOT_SRC_DIR}/${projectName}"
+        COMMENT "Installing README file for ${projectName}" )
 
     ADD_CUSTOM_TARGET ( ${projectName}-readme ALL 
         DEPENDS "${ROOT_BIN_DIR}/${projectName}/README" )
 
-    ADD_DEPENDENCIES ( ${projectName} ${projectName}-readme )
+    ADD_DEPENDENCIES ( ${projectName}-executable ${projectName}-readme )
 
 ENDMACRO ( ADD_README_TARGET projectName )
 
@@ -250,3 +256,32 @@ MACRO ( ADD_HSC2HS_TARGET projectName hscFileSansExtension )
         ${projectName}-${hscFileSansExtension} )
 
 ENDMACRO ( ADD_HSC2HS_TARGET projectName hscFileSansExtension )
+
+# For libraries that are part of the same project only...
+MACRO ( INSTALL_CPP_RELOCATABLE_LIBRARY projectName library )
+
+    GET_RELOCATABLE_LIBRARY_NAME( ${library} ${library}_NAME )
+    GET_EXECUTABLE_NAME ( "${ROOT_BIN_DIR}/${projectName}/bin/${projectName}" ${projectName}_EXECUTABLE )
+
+    ADD_CUSTOM_COMMAND (
+        OUTPUT "${ROOT_BIN_DIR}/${projectName}/bin/${${library}_NAME}"
+        COMMAND "cp"
+        ARGS "-v"
+            "${ROOT_BIN_DIR}/${library}/${${library}_NAME}" "${ROOT_BIN_DIR}/${projectName}/bin/${${library}_NAME}"
+        DEPENDS ${library} "${${projectName}_EXECUTABLE}"
+        WORKING_DIRECTORY "${ROOT_SRC_DIR}/${projectName}"
+        COMMENT "Installing library ${${library}_NAME} for ${projectName}" )
+
+    ADD_CUSTOM_TARGET ( ${projectName}-${library} ALL 
+        DEPENDS ${projectName}-executable )
+
+    ADD_DEPENDENCIES ( ${projectName} 
+        ${projectName}-${library} )
+
+ENDMACRO ( INSTALL_CPP_RELOCATABLE_LIBRARY projectName library )
+
+MACRO ( ADD_HASKELL_EXECUTABLE_DEPENDENCIES projectName )
+    
+    ADD_DEPENDENCIES ( ${projectName}-executable ${ARGN} )
+
+ENDMACRO ( ADD_HASKELL_EXECUTABLE_DEPENDENCIES projectName )
