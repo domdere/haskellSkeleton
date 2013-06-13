7cc90421863b54acf6e0e12f15b08f2b937fa8fd
Dom De Re <domdere@domdere.com>
2013-06-14 01:36:45 +1000

    Got the arg passing in a state which i can at least use in function type signatures, but not all the marshalling is sorted out yet

--- Foreign/C/ArgPassing.hs
+++ Foreign/C/ArgPassing.hs
@@ -4,6 +4,8 @@
 module Foreign.C.ArgPassing where
 
 import Foreign.C.String
+import Foreign.Marshal.Alloc
+import Foreign.Marshal.Utils
 import Foreign.Ptr
 import Foreign.Storable
 import Foreign 
@@ -130,5 +132,33 @@ instance Storable InOutCString where
         poke (p `plusPtr` 0) cstr
         return ()
 
+-- simple things we want to do with these data types
+
+-- | Marshals a value into a Ptr and wraps that in the InPtr type
+withInPtr :: (Storable a) => a -> (InPtr a -> IO b) -> IO b
+withInPtr value action = with value (\ptr -> action (InPtr ptr))
+
+wrapOutPtrIO :: (Storable a) => (OutPtr a -> IO b) -> Ptr a -> IO (a, b)
+wrapOutPtrIO action ptr = do
+    returnVal <- action $ OutPtr ptr
+    outVal <- peek ptr
+    return (outVal, returnVal)
+
+-- TODO (Dom De Re) : Clean this up, this is probably a hint that these ptr types
+-- should be part of a common class, but I'll play around with them a bit more before I
+-- decide whether it should be a new type class or an existing one like a Comonad or something.
+wrapInOutPtrIO :: (Storable a) => (InOutPtr a -> IO b) -> Ptr a -> IO (a, b)
+wrapInOutPtrIO action ptr = do
+    returnVal <- action $ InOutPtr ptr
+    outVal <- peek ptr
+    return (outVal, returnVal)
 
 
+-- | Marshals a value into a Ptr and wraps that in the OutPtr type, runs the action
+-- | and then retrieve the output value with Storable.peek
+withOutPtr :: (Storable a) => (OutPtr a -> IO b) -> IO (a, b)
+withOutPtr action = alloca (wrapOutPtrIO action) 
+
+-- | Combines In and OutPtr into InOutPtr
+withInOutPtr :: (Storable a) => a -> (InOutPtr a -> IO b) -> IO (a, b)
+withInOutPtr value action = with value $ wrapInOutPtrIO action
