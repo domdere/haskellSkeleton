68637d0506ca03a17c4a5f594ae93edd3f8a1c69
Dom De Re <domdere@domdere.com>
2013-07-02 22:36:58 +1000

    Adding a string array marshalling function

---
+++ Foreign/C/StringArray.hs
@@ -0,0 +1,20 @@
+module Foreign.C.StringArray where
+
+import qualified Data.ByteString as BS
+import Foreign.C.String (CString, withCString)
+import Foreign.Marshal.Array (withArray)
+import Foreign.Ptr (nullPtr, plusPtr, Ptr)
+import Foreign.Storable (poke)
+
+withCStringArray :: [BS.ByteString] -> (Ptr CString -> IO a) -> IO a
+withCStringArray strings action =
+    let arrayLength                                 = length strings
+        nullPtrs                                    = take arrayLength $ repeat nullPtr
+        initialiseAndRun array _ [] action          = action array
+        initialiseAndRun array offset (x:xs) action = do
+            BS.useAsCString x $ \cstr -> do
+                poke (array `plusPtr` offset) cstr
+                initialiseAndRun array (offset + 1) xs action
+    in do
+        withArray nullPtrs $ \array -> do
+            initialiseAndRun array 0 strings action
