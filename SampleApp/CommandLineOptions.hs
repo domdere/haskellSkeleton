144a9b4b3e24a097003cd7aff00f940adab6009c
Dom De Re <domdere@domdere.com>
2013-07-13 18:03:07 +1000

    removed some untidy comments

--- SampleApp/CommandLineOptions.hs
+++ SampleApp/CommandLineOptions.hs
@@ -16,26 +16,3 @@ optparser :: Parser CommandLineOptions
 optparser = (mkCommandLineOptions "./test.log" LevelInfo)
     <$> optionalLogFileOpt
     <*> optionalLogLevelOpt
-
-{-
-logFileOpt :: Parser FilePath
-logFileOpt = strOption
-    (long "logfile"
-    <> metavar "FILE"
-    <> help "Path to Log File, default=['./test.log']")
-
-optionalLogFileOpt :: Parser (Maybe FilePath)
-optionalLogFileOpt = optional logFileOpt
-
-logLevelStrOption :: Parser String
-logLevelStrOption = strOption
-    (long "loglevel"
-    <> metavar "LOGLEVEL"
-    <> help "The minimum log level that gets logged, one of: LogDebug, LogInfo, LogWarn, LevelError, default=['LogInfo']")
-
-logLevelOpt :: Parser LogLevel
-logLevelOpt = read `fmap` logLevelStrOption
-
-optionalLogLevelOpt :: Parser (Maybe LogLevel)
-optionalLogLevelOpt = optional logLevelOpt
--}
